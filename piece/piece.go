e773dbaaf6c0fb9371d0439d07cfb3a13b3fb648
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-09-09 19:32:38 -0400

    create func to get random shape

--- piece/piece.go
+++ piece/piece.go
@@ -1,14 +1,15 @@
 package piece
 
-var shapes = map[string][][]int{
-	"1x1": {{1}},
-	"2x2": {{1, 1}, {1, 1}},
+import (
+	"math/rand"
+)
+
+var shapes = [][][]int{
+	{{1}},
+	{{1, 1}, {1, 1}},
 }
 
-type Piece struct {
-	Shape string
-}
-
-func (p *Piece) GetPattern() [][]int {
-	return shapes[p.Shape]
+func GetRandomShape () [][]int {
+	randomNumber := rand.Intn(len(shapes))
+	return shapes[randomNumber]
 }
