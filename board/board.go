ba62ad7c42026f12fbb73db98e81925bc07ebbb8
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-09-11 22:57:54 -0400

    remove unused code

--- board/board.go
+++ board/board.go
@@ -4,24 +4,8 @@ import (
 	"errors"
 )
 
-// ANSI escape codes for colors
-const (
-	Reset  = "\033[0m"
-	Red    = "\033[31m"
-	Green  = "\033[32m"
-	Yellow = "\033[33m"
-	Blue   = "\033[34m"
-	Purple = "\033[35m"
-	Cyan   = "\033[36m"
-	White  = "\033[37m"
-)
-
 type Board struct {
 	Grid [9][9]bool
-}
-
-type Diff struct {
-	ColorGrid [9][9]string
 }
 
 type Cell struct {
@@ -50,23 +34,6 @@ func (b *Board) ToString() string {
 			} else {
 				str += "□ "
 			}
-		}
-		str += "\n"
-	}
-	return str
-}
-
-func (b *Board) ToDiffString(diff Diff) string {
-	str := ""
-	for rowI := range b.Grid {
-		for colI := range b.Grid[rowI] {
-			str += diff.ColorGrid[rowI][colI]
-			if b.Grid[rowI][colI] {
-				str += "▣ "
-			} else {
-				str += "□ "
-			}
-			str += Reset
 		}
 		str += "\n"
 	}
@@ -115,24 +82,6 @@ func (b *Board) Evaluate() int {
 	b.removeCells(completedCells)
 
 	return completionCount
-}
-
-func (b *Board) Diff(prev Board) Diff {
-	diff := Diff{ColorGrid: [9][9]string{}}
-
-	for rowI := range b.Grid {
-		for colI := range b.Grid[rowI] {
-			if b.Grid[rowI][colI] && !prev.Grid[rowI][colI] {
-				diff.ColorGrid[rowI][colI] = Green
-			} else if !b.Grid[rowI][colI] && prev.Grid[rowI][colI] {
-				diff.ColorGrid[rowI][colI] = Red
-			} else {
-				diff.ColorGrid[rowI][colI] = White
-			}
-		}
-	}
-
-	return diff
 }
 
 func (b *Board) evaluateRows() []Cell {
